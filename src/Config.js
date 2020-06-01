2f01dea853ecbae055cc3155c32a673109d05dea
rongt2-1861545 <tianai.r.34289@gmail.com>
2020-06-01 12:32:48 -0700

    update

---
+++ src/Config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnfmx_XOl83x0kNqEl9_AIS7l_L1yzMHs",
+  authDomain: "goallabdb.firebaseapp.com",
+  databaseURL: "https://goallabdb.firebaseio.com",
+  projectId: "goallabdb",
+  storageBucket: "goallabdb.appspot.com",
+  messagingSenderId: "962233652100",
+  appId: "1:962233652100:web:a8f3f54aaf5d917c1f43d1",
+  measurementId: "G-QFC7WEL3JR"
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import firebase from "firebase/app";
+import firebaseConfig from "./Config"
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector('body'));
