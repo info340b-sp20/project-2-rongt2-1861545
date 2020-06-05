bfb09c4e5c8fdbfdac8919ab9cfca03d1c1d397a
rongt2-1861545 <tianai.r.34289@gmail.com>
2020-06-05 13:41:23 -0700

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,45 +6,37 @@ import firebase from 'firebase/app';
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {todos:[]};
+    this.state = {
+      todos:[]
+    };
   }
 
-componentDidMount() {
-  let todosRef = firebase.database().ref('todos');
-  
-  todosRef.on('value', (snapshot) => {
-
-    let data = snapshot.val();
-    if (data) {
-      let todoKeys = Object.keys(data);
-      let todoArray = todoKeys.map((key) => {
-        let todoObj = data[key];
-        todoObj.id = key;
-        return todoObj;
-      })
-      this.setState({ todos: todoArray });
-    } else {
-      return
-    }
-  })
-}
+  componentDidMount() {
+    let todosRef = firebase.database().ref('todos');
+    
+    todosRef.on('value', (snapshot) => {
+      let data = snapshot.val();
+      if (data) {
+        let todoKeys = Object.keys(data);
+        let todoArray = todoKeys.map((key) => {
+          let todoObj = data[key];
+          todoObj.id = key;
+          return todoObj;
+        })
+        this.setState({ todos: todoArray });
+      } else {
+        return
+      }
+    })
+  }
 
   render() {
     if(!this.state.todos) return null; //if no todos, don't display
     
     let todoItems = this.state.todos.map((todo) => {
-      let ref = firebase.database().ref('todos/' + todo.id).child("type")
-      let data;
-      let item = async () => {
-        let snapshot = await ref.once("value");
-        data = snapshot.val();
-        // console.log(data === this.props.containerType)
-        if  (data === this.props.containerType) {
-          // console.log(<TodoItem  key={todo.id} todo={todo} currentUser={this.props.currentUser} />)
-          return <TodoItem  key={todo.id} todo={todo} currentUser={this.props.currentUser} />
-        }
+      if (todo.type === this.props.containerType) {
+        return <TodoItem  key={todo.id} todo={todo} currentUser={this.props.currentUser} />
       }
-      return item;
     })  
 
     return (
@@ -59,11 +51,16 @@ componentDidMount() {
 class TodoItem extends Component {
   constructor(props) {
     super(props);
-    this.toggleComplete = this.toggleComplete.bind(this);
+    this.state = {
+      btnColor: "white",
+      strike: ""
+    };
+    // this.toggleComplete = this.toggleComplete.bind(this);
   }
 
-  toggleComplete = (event) => {
+  toggleComplete = () => {
     let ref = firebase.database().ref('todos/' + this.props.todo.id).child("complete")
+
     ref.once('value').then((snapshot) => {
       if(snapshot.val() === true) {
         ref.set(false);
@@ -71,33 +68,31 @@ class TodoItem extends Component {
         ref.set(true);
       }
     });
-    if (window.getComputedStyle(event.target, null).getPropertyValue("background-color") === "rgb(255, 255, 255)") {
-      event.target.style.backgroundColor = "#3370ff";
+    if(this.props.todo.complete === false) {
+      this.setState({
+        btnColor: "white",
+        strike: "line-through"
+      })
     } else {
-      event.target.style.backgroundColor = "#FFF";
+      this.setState({
+        btnColor: "blue",
+        strike: ""
+      })
     }
   }
- 
-  // toggleStrike = (event) => {
-  //   if (window.getComputedStyle(event.target, null).getPropertyValue("text-decoration-line") !== "line-through") {
-  //     event.target.style.textDecorationLine = "line-through";
-  //   } else {
-  //     // event.target.style.backgroundColor = "#FFF";
-  //   }
-  // }
 
   render() {
     let todo = this.props.todo;
-    
     return ( 
       <div className="draggable">
         <div className="todo-item todo" draggable="false">
           <div className="icon">
             <button className="circle"
-                    onClick={(e) => { this.toggleComplete(e) }}
+                    onClick={this.toggleComplete} 
+                    style={{backgroundColor: this.state.btnColor}}
             />
           </div>
-          <input className="ipt-todo ipt-all input" type="text" value={todo.text} />
+          <input className="ipt-todo ipt-all input" type="text" value={todo.text} style={{textDecorationLine: this.state.strike}}/>
         </div>
          <div className="is-divider" />
       </div>
