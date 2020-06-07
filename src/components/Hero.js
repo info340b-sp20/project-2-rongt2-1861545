d2f24f060e96b504c122ba78d09350d0793eb5de
Nicholas Xu <tichx@uw.edu>
2020-06-07 17:44:08 +0800

    fix coloring

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,8 +14,8 @@ class Hero extends Component {
       <section className="hero is-medium is-primary is-bold">
         <div className="hero-body bg-img img-responsive">
             <div className="container">
-    <h1 className="title">Good Afternoon, {this.name}</h1>
-                <h2 className="subtitle" id="greeter">Yesterday you said tomorrow...</h2>
+                <h1 id="hero-title" className="title">Good Afternoon, {this.name}</h1>
+                <h2 id="hero-subtitle" className="subtitle">What's your focus today?</h2>
             </div>
         </div>
       </section>
